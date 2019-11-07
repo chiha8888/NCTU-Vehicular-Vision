18acf7135455b45f7c9318cf243089d74051e286
yan-roo <49684416+yan-roo@users.noreply.github.com>
2019-11-08 02:01:22 +0800

    Update main & Update README & Upload HW1 report

--- HW01/main.py
+++ HW01/main.py
@@ -3,30 +3,59 @@ import os
 import matplotlib.pyplot as plt
 from postprocess import postprocess
 from preprocess import preprocess
-gt=os.listdir(os.path.join('HW1_dataset','groundtruth'))
+
+
+# Path Setting
+print("Use Default Path Setting Press 0")
+print("Custom Path Setting Press 1")
+setting = int(input(">>>"))
+if setting == 1:
+    ground_truth_path = str(input("Your Ground Truth Data Path:"))
+    input_data_path = str(input("Your Input Data Path:"))
+    output_data_path = str(input("Your Output Save Path:"))
+    ROI_path = str(input("Your ROI Path & Name:"))
+elif setting == 0:
+    ground_truth_path = "./HW1_dataset/groundtruth"
+    input_data_path = "./HW1_dataset/input"
+    output_data_path = "./EvaluationV2/results"
+    ROI_path = "./HW1_dataset/ROI.bmp"
+else:
+    os._exit(0)
+
+# Read Files
+gt=os.listdir(os.path.join(ground_truth_path))
 gt=[int(path[5:8]) for path in gt]
 
-backSub=cv2.bgsegm.createBackgroundSubtractorMOG(history=260,backgroundRatio=0.4)
-capture=cv2.VideoCapture(cv2.samples.findFileOrKeep(os.path.join(os.path.join('HW1_dataset','input'),'in%06d.png')))
-if capture.isOpened()==0:
-    print('unable to open file')
+mask = cv2.imread(os.path.join(ROI_path), 0)
+
+backSub = cv2.bgsegm.createBackgroundSubtractorMOG(history=260, backgroundRatio=0.4)
+capture = cv2.VideoCapture(cv2.samples.findFileOrKeep(os.path.join(os.path.join(input_data_path), 'in%06d.png')))
+if capture.isOpened() == 0:
+    print('Unable to Open File')
     exit(0)
 
-count=1
+
+count = 1
 while True:
-    ret,frame=capture.read()
+    ret, frame = capture.read()
     if frame is None:
         break
 
-    fgMask=backSub.apply(preprocess(frame))
-    fgMask=postprocess(fgMask,25,20)
+    fgMask = backSub.apply(preprocess(frame, mask))
+    fgMask = postprocess(fgMask, 25, 20)
 
-    cv2.imshow('frame',frame)
-    cv2.imshow('fgMask',fgMask)
+    cv2.imshow('frame', frame)
+    cv2.imshow('fgMask', fgMask)
     plt.show()
     cv2.waitKey(10)
     #save result (comparing with ground_truth)
     if count in gt:
-        cv2.imwrite(os.path.join('EvaluationV2',os.path.join('results','{:0>6d}.png'.format(count))),fgMask)
+        if not os.path.exists(output_data_path):
+            os.makedirs(output_data_path)
+        cv2.imwrite(os.path.join(output_data_path, '{:0>6d}.png'.format(count)), fgMask)
 
     count += 1
+
+print ("Finish! Press Any Keys to Exit")    
+cv2.waitKey(0)
+cv2.destroyAllWindows()
--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 # NCTU-Vehicular-Vision
 
-### HW01
-調一下postprocess那邊的erode,dilate組合  
+## HW01 (Foreground Detection)
 
-Recall Specificity	FPR		FNR		PWC		Precision	FMeasure  
-0.78453	0.99509	0.00491	0.21547	2.67583	0.94871	0.85885
+postprocess  erode, dilate combination
+
+
+Recall  | Specificity |   FPR   |   FNR   |   PWC   | Precision | FMeasure
+--------|:-----------:|--------:| -------:|--------:|----------:|-----------
+0.78453 |   0.99509   | 0.00491 | 0.215472| 2.67583 |  0.94871  |  0.85885
+
+
+## Prerequisites
+
+- Python 3.x
+- opencv-contrib-python     4.x
+- opencv-python             4.x       
