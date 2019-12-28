a129ec163c128c8cae73dae7a3a67164ada84419
dorawu <chiha8888@gmail.com>
2019-12-28 20:54:30 +0800

    update main2.py in Tello_Video

--- Tello-Python/Tello_Video/main2.py
+++ Tello-Python/Tello_Video/main2.py
@@ -1,27 +1,49 @@
-import tello
 import cv2
+import numpy as np
+import tello
 
 if __name__=='__main__':
     drone=tello.Tello('',8889)
+    out=cv2.VideoWriter('output4.avi',cv2.VideoWriter_fourcc(*'XVID'),100,(960,720))
     while True:
+
         # get frame
         frame=drone.read()
+        if frame is None:
+            continue
 
-        # s key to take off
-        if cv2.waitKey(1) & 0xFF == ord('s'):
+        key=cv2.waitKey(1)
+        if key==27:  # key 'esc'
+            drone.land()
+            break
+        elif key==108:  # key 'l'
+            drone.land()
+        elif key==116:  # key  't'
             drone.takeoff()
-		# q key to land 
-		if cv2.waitKey(1) & 0xFF == ord('q'):
-	    	drone.land()
-			break
+        elif key==119:  # key 'w'
+            drone.move_forward(0.3)
+        elif key==115:  # key 's'
+            drone.move_backward(0.3)
+        elif key==97:  # key 'a'
+            drone.rotate_ccw(20)
+        elif key==100:  # key 'd'
+            drone.rotate_cw(20)
+        elif key==113:  # key 'q'
+            drone.move_up(0.3)
+        elif key==101:  # key 'e'
+            drone.move_down(0.3)
 
-        # process by frame
-        cv2.imshow('frame', frame)
-        #cv2.imwrite(p,cv2.cvtColor(frame,cv2.COLOR_RGB2BGR))
-	'''
+        # process frame
+        frame=cv2.cvtColor(frame,cv2.COLOR_RGB2BGR)
+        cv2.imshow('f', frame)
+        out.write(frame)
+
+        '''
         # control drone
         drone.rotate_cw(20)
         drone.rotate_ccw(20)
-        drone.move_forward(0.3)
-	'''
-	
+        drone.move_forward(0.3)s
+        '''
+
+    out.release()
+    cv2.destroyAllWindows()
